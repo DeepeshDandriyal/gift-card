27c93387b96901006fb2222add61433983341f25
Deepesh Dandriyal <deepeshdandriyal1@gmail.com>
2023-07-14 22:20:58 +0530

    padding added for cover-page

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,15 @@ import { FriendContext } from "../context/YearContext";
 
 const Header = () => {
   const { setDescription, setImage } = useContext(FriendContext);
-  const [friend, setFriend] = useState("Aishwarya");
+  const [friend, setFriend] = useState("Select");
 
   switch (friend) {
+    case "Select":
+      setDescription(
+        "Happy birthday, Beautiful❤️! May your day be filled with joy, laughter, and countless wonderful memories to cherish! Check out the messages and memories shared by your friends from select toggle above😜😜"
+      );
+      setImage("../Images/Select.jpg");
+      break;
     case "Aishwarya":
       setDescription(
         "We might have less pictures together but shared beautiful bond of friendship ,gald to met such a sweet innocent soul,Keep up ur innocence May u manifest all your goals nd desire Happy birthday kalpana❤"
@@ -14,11 +20,15 @@ const Header = () => {
       setImage("../Images/Aishwarya.jpeg");
       break;
     case "Kaprwan":
-      setDescription("guyss its 2015");
+      setDescription(
+        "Happy birthday to Kalpana! As the years unfold, may your dreams come true and may every day be brighter than the last.🥳🥳"
+      );
       setImage("../Images/Kaprwan.jpeg");
       break;
     case "Ridhima":
-      setDescription("lorem ipsum");
+      setDescription(
+        "Sending you warmest wishes on your birthday, Kalpana! May this new chapter of your life be filled with exciting adventures, beautiful memories, and the fulfillment of all your heart's desires"
+      );
       setImage("../Images/Ridhima.jpeg");
       break;
     case "Shikha":
@@ -47,7 +57,7 @@ const Header = () => {
       break;
     case "Shubhi":
       setDescription(
-        "Hy Kalpana wish u a very happy birthday dear 💕.hope all your wishes come true ❤"
+        "Happy birthday to my cute friend and the one who always has my back! Thank you for being there through thick and thin. May this year bring you abundant love, health, and prosperity.❤️❤️"
       );
       setImage("../Images/Shubhi.jpeg");
       break;
@@ -56,6 +66,18 @@ const Header = () => {
         "Happy birthday, Kalpu! Here's to a fantastic year ahead and an extraordinary future filled with love, success, and all the beautiful things life has to offer. Cheers! 🥳🎉"
       );
       setImage("../Images/Kiran (1).jpeg");
+      break;
+    case "Deepesh":
+      setDescription(
+        "Happy birthday to someone who brings so much positivity and light into my life❤️❤️ .May your special day be as incredible as you are, and may the future hold boundless opportunities for you🥳🥳"
+      );
+      setImage("../Images/Deepesh.jpg");
+      break;
+    case "Ajay":
+      setDescription(
+        "Sending you warmest wishes on your birthday, Kalpana! May this new chapter of your life be filled with exciting adventures, beautiful memories, and the fulfillment of all your heart's desires"
+      );
+      setImage("../Images/Ajay.jpg");
       break;
     case "Aadi":
       setDescription(
@@ -86,15 +108,18 @@ const Header = () => {
           setFriend(e.target.value);
         }}
       >
+        <MenuItem value={"Select"}>Select</MenuItem>
+        <MenuItem value={"Aishwarya"}>Aishwarya</MenuItem>
         <MenuItem value={"Kiran"}>Kiran</MenuItem>
         <MenuItem value={"Shubhi"}>Shubhi</MenuItem>
-        <MenuItem value={"Aishwarya"}>Aishwarya</MenuItem>
+
         <MenuItem value={"Shikha"}>Shikha</MenuItem>
-        <MenuItem value={"Deepesh"}>Deepesh</MenuItem>
+
         <MenuItem value={"Ajay"}>Ajay</MenuItem>
         <MenuItem value={"Samiksha"}>Samiksha</MenuItem>
         <MenuItem value={"Akhil"}>Akhil</MenuItem>
         <MenuItem value={"Ridhima"}>Ridhima</MenuItem>
+        <MenuItem value={"Deepesh"}>Deepesh</MenuItem>
         <MenuItem value={"Aadi"}>Aadi</MenuItem>
         <MenuItem value={"Rayal"}>Rayal</MenuItem>
         <MenuItem value={"Kaprwan"}>Kaprwan</MenuItem>
